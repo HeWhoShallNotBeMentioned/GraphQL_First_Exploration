0034ea986b1ba4a888b11eedbaf8a6b641b45817
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-17 16:54:58 -0600

    add first query to server

--- src/index.js
+++ src/index.js
@@ -3,13 +3,40 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }`;
 
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => `This is the API of Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url,
   },
 };
+
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial on GraphQL',
+  },
+  {
+    id: 'link-1',
+    url: 'www.facebook.com',
+    description: 'The creators of GraphQL',
+  },
+];
 
 const server = new GraphQLServer({
   typeDefs,
