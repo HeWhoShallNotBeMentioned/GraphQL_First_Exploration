072f539fe9bca2f3fa1dd9e546537814cef2ec62
Chris Underwood <cunderwoodmn@gmail.com>
2019-01-18 11:58:36 -0600

    connected prisma client with the database and graphql

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial on GraphQL',
-  },
-  {
-    id: 'link-1',
-    url: 'www.facebook.com',
-    description: 'The creators of GraphQL',
-  },
-];
-
-let idCount = links.length;
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: () => (root, args, context, info) => {
+      return context.prisma.links();
+    },
   },
   Mutation: {
     // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-      return link;
+        description: args.description,
+      });
     },
   },
 };
@@ -37,5 +22,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
